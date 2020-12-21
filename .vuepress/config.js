3ee27d2953f236a9b03dc394f5dd7a6af4b4e497
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2020-12-22 00:36:41 +0300

    Init repo

---
+++ .vuepress/config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  title: 'Awssat',
+  description: 'Web/mobile development agency. Developing high class applications',
+  dest: './docs',
+  themeConfig: {
+    logo: '/icons/logo.png',
+    displayAllHeaders: true,
+    sidebar: require('./contentFiles.js'),
+    repo: 'awssat',
+    docsRepo: 'awssat/website',
+    editLinks: true,
+    editLinkText: 'Help us improve this page!',
+    lastUpdated: 'Last Updated'
+  }
+}
